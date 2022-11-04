cff64983f4d8f4c0d36a5656d8d3a3eda65700c8
krzysztofkucharzyk <krzysztofkucharzyk@gmail.com>
2022-11-04 15:20:13 +0100

    STEP 2.v0.1

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Axios from 'axios'
 
 const Home = () => {
+
+  const [posts, setPosts] = useState([]);
+
+  const getLatestPosts = () => {
+    Axios.post('http://akademia108.pl/api/social-app/post/latest')
+      .then(res => {
+        console.log("Pobieranie danych", res.data)
+        setPosts(res.data)
+      })
+      .catch(error => console.log(error))
+  }
+
+  useEffect(() => {
+    getLatestPosts();
+  }, [])
+
+
+
   return (
     <section className='Home'>
       <h2>Home</h2>
+
+      <div className='postList'>
+        {posts.map((post) => (
+          <div key={post.id}>
+            <p> {post.content}</p>
+          </div>
+        ))}
+      </div>
+
+
+
+
+
     </section>
   )
 }
