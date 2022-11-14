260bf6ca5ea16b7c0dda1c2112e0b75339a88e1e
krzysztofkucharzyk <krzysztofkucharzyk@gmail.com>
2022-11-14 10:35:45 +0100

    STEP 5.v.0.4

--- src/components/AppNav.js
+++ src/components/AppNav.js
@@ -1,13 +1,24 @@
+import axios from 'axios';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './AppNav.css';
 
 const AppNav = (props) => {
 
-const handleLogout = () => {
-  localStorage.removeItem('user');
-  props.setUser();
-}
+  const handleLogout = () => {
+
+    axios.post('https://akademia108.pl/api/social-app/user/logout')
+      .then(res => {
+        let resData = res.data
+        if (resData.message) {
+          // localStorage.removeItem('user');
+          localStorage.setItem('user', null);
+          props.setUser(null);
+        }
+        console.log(res.data)
+      })
+      .catch(error => console.log(error))
+  }
 
   return (
     <>
@@ -18,19 +29,19 @@ const handleLogout = () => {
               <Link to="/">Home</Link>
             </li>
             {!props.user && (
-            <li>
-              <Link to="/Login">Login</Link>
-            </li>
+              <li>
+                <Link to="/Login">Login</Link>
+              </li>
             )}
             {!props.user && (
-            <li>
-              <Link to="/SignUp">SignUp</Link>
-            </li>
+              <li>
+                <Link to="/SignUp">SignUp</Link>
+              </li>
             )}
             {props.user && (
-            <li>
-              <Link to="/" onClick={handleLogout}>Log Out</Link>
-            </li>
+              <li>
+                <Link to="/" onClick={handleLogout}>Log Out</Link>
+              </li>
             )}
           </ul>
         </nav>
