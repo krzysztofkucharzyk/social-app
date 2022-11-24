de9204d87721c8fe1489705fb6bef052e7225090
Krzysztof Kucharzyk <krzysztofkucharzyk@gmail.com>
2022-11-24 19:44:18 +0100

    STEP 8.v.0.1

--- src/components/Post.js
+++ src/components/Post.js
@@ -34,10 +34,6 @@ const Post = (props) => {
         else if (resData.errors) {
             alert("Invalid Operation. You have not sufficient permissions")
         }
-        else if (resData.errors) {
-            alert("Invalid Operation. You have not sufficient permissions")
-            setDeleteModalDisplay(false)
-        }
       })
       .catch((error) => {
         console.log(error);
