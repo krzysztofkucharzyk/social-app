d04e1cc5d8cb58f0877e66dad3311430e9e8fd2e
krzysztofkucharzyk <krzysztofkucharzyk@gmail.com>
2022-11-25 08:29:46 +0100

    ...

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,24 +1,29 @@
-import React from 'react'
-import "./Modal.css"
+import React from "react";
+import "./Modal.css";
 
-
-const Modal = ({ closeModal, deletePost }) => {
+const Modal = ({closeModal, deletePost}) => {
   return (
     <div className="container">
       <div className="card">
         <div className="card_body_modal">
           <div className="card_body_modal_title">
-            <h2>Are you sure you want to delete a post
-              ?</h2>
+            <h2>Are you sure you want to delete a post ?</h2>
           </div>
-          <div className='card_body_modal_buttons'>
-          <button onClick={deletePost} className="button button_yes">YES</button>
-          <button onClick={() => closeModal(false)} className="button button_no">NO</button>
+          <div className="card_body_modal_buttons">
+            <button onClick={deletePost} className="button button_yes">
+              YES
+            </button>
+            <button
+              onClick={() => closeModal(false)}
+              className="button button_no"
+            >
+              NO
+            </button>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Modal
+export default Modal;
