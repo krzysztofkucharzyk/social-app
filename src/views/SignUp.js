f0fd31220cb62ddac8636f09de7003d22009a6f3
krzysztofkucharzyk <krzysztofkucharzyk@gmail.com>
2022-12-02 13:49:21 +0100

    STEP 9.v.0.3

--- src/views/SignUp.js
+++ src/views/SignUp.js
@@ -1,95 +1,212 @@
-import axios from 'axios';
-import React, { useState } from 'react';
+import axios from "axios";
+import React, {useState} from "react";
 import "./SignUp.css";
 
-const username_regex = /^[a-zA-Z][a-zA-Z0-9-_]{4,23}[^\s]*/;
+// const username_regex = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}[^\s]*$/;
+// const username_regex = /^(?=.{3,20}$)(?![^\s])(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/;
 const email_regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/;
-const password_regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!#@$%])(?=.{8,})$/
+// const password_regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!#@$%])(?=.{5,})$/;
 
-const SignUp = (props) => {
-
-
+const SignUp = () => {
   const [signUpMessage, setSignUpMessage] = useState("");
+
   const [data, setData] = useState({
     username: "",
     email: "",
     password: "",
+    repassword: "",
+  });
+
+  const [errors, setErrors] = useState({
+    username: "",
+    email: "",
+    password: "",
+    repassword: "",
   });
 
   const handleInputChange = (e) => {
-    setData({ ...data, [e.target.name]: e.target.value });
+    setData({...data, [e.target.name]: e.target.value});
   };
 
-  const onSubmit = (e) => {
+  const validate = (e) => {
     e.preventDefault();
 
+    let validateErrors = {
+      username: false,
+      email: false,
+      password: false,
+      repassword: false,
+    };
+
+    // Username
+    if (data.username.trim().length < 4) {
+      validateErrors.username = true;
+      setErrors((prevErrors) => {
+        return {
+          ...prevErrors,
+          username: "Username should have at least 4 characters",
+        };
+      });
+    } else if (!/^[^\s]*$/.test(data.username.trim())) {
+      validateErrors.username = true;
+      setErrors((prevErrors) => {
+        return {...prevErrors, username: "Username cannot contain whitespaces"};
+      });
+    } else {
+      validateErrors.username = false;
+      setErrors((prevErrors) => {
+        return {...prevErrors, username: ""};
+      });
+    }
+
+    //Email
+    if (
+      !email_regex.test(data.email.trim()) ||
+      data.email.trim().length === 0
+    ) {
+      validateErrors.email = true;
+      setErrors((prevErrors) => {
+        return {...prevErrors, email: "This is no valid email"};
+      });
+    } else {
+      validateErrors.email = false;
+      setErrors((prevErrors) => {
+        return {...prevErrors, email: ""};
+      });
+    }
+
+    //Password
+    if (data.password.trim().length < 6) {
+      validateErrors.password = true;
+      setErrors((prevErrors) => {
+        return {
+          ...prevErrors,
+          password: "Password should have at least 6 characters",
+        };
+      });
+    } else if (!/^[^\s]*$/.test(data.password.trim())) {
+      validateErrors.password = true;
+      setErrors((prevErrors) => {
+        return {...prevErrors, password: "Password cannot contain whitespaces"};
+      });
+    } else if (!/\d/.test(data.password.trim())) {
+      validateErrors.password = true;
+      setErrors((prevErrors) => {
+        return {
+          ...prevErrors,
+          password: "Password should have at least 1 number",
+        };
+      });
+    } else if (!/[!@#$%]/.test(data.password.trim())) {
+      validateErrors.password = true;
+      setErrors((prevErrors) => {
+        return {
+          ...prevErrors,
+          password:
+            "Password should have at least 1 of the special characters like: !, @, #, $, %",
+        };
+      });
+    } else {
+      validateErrors.password = false;
+      setErrors((prevErrors) => {
+        return {...prevErrors, password: ""};
+      });
+    }
+
+    //Repeat password
+    if (
+      data.repassword.trim() !== data.password.trim() ||
+      data.repassword.trim().length === 0
+    ) {
+      validateErrors.repassword = true;
+      setErrors((prevErrors) => {
+        return {
+          ...prevErrors,
+          repassword: "Password do not match",
+        };
+      });
+    } else {
+      validateErrors.repassword = false;
+      setErrors((prevErrors) => {
+        return {...prevErrors, repassword: ""};
+      });
+    }
+  };
+
+  const handleSubmit = () => {
     let newUser = {
       username: data.username,
       email: data.email,
       password: data.password,
     };
 
-    axios.post('https://akademia108.pl/api/social-app/user/signup', JSON.stringify(newUser))
+    axios
+      .post(
+        "https://akademia108.pl/api/social-app/user/signup",
+        JSON.stringify(newUser)
+      )
       .then((res) => {
         console.log(res.data);
         let resData = res.data;
-
-
       })
       .catch((error) => {
         console.log(error);
-      })
-    }
+      });
+  };
 
-    return (
-      <section className="SignUp">
-        <div className="signup-form">
-          <form onSubmit={onSubmit}>
-            <h1>Sign Up</h1>
-            <div className="content">
-              <div className="input-field">
-                <input
-                  name="username"
-                  value={data["username"]}
-                  type="text"
-                  placeholder="Username"
-                  onChange={handleInputChange}
-                />
-              </div>
-              <div className="input-field">
-                <input
-                  name="email"
-                  value={data["email"]}
-                  type="text"
-                  placeholder="Email"
-                  onChange={handleInputChange}
-                />
-              </div>
-              <div className="input-field">
-                <input
-                  name="password"
-                  type="text"
-                  placeholder="Password"
-                  onChange={handleInputChange}
-                />
-              </div>
-              <div className="input-field">
-                <input
-                  name="repassword"
-                  type="password"
-                  placeholder="Repeat password"
-                  onChange={handleInputChange}
-                />
-              </div>
+  return (
+    <section className="SignUp">
+      <div className="signup-form">
+        <form onSubmit={validate}>
+          <h1>Sign Up</h1>
+          <div className="content">
+            <div className="input-field">
+              <input
+                name="username"
+                value={data["username"]}
+                type="text"
+                placeholder="Username"
+                onChange={handleInputChange}
+              />
+              <span>{errors.username}</span>
             </div>
-            <div className="action">
-              <button>SIGN UP</button>
+            <div className="input-field">
+              <input
+                name="email"
+                value={data["email"]}
+                type="text"
+                placeholder="Email"
+                onChange={handleInputChange}
+              />
+              <span>{errors.email}</span>
             </div>
-          </form>
-        </div>
-        <div className="error_msg">{signUpMessage}</div>
-      </section>
-    )
-  }
+            <div className="input-field">
+              <input
+                name="password"
+                type="text"
+                placeholder="Password"
+                onChange={handleInputChange}
+              />
+              <span>{errors.password}</span>
+            </div>
+            <div className="input-field">
+              <input
+                name="repassword"
+                type="password"
+                placeholder="Repeat password"
+                onChange={handleInputChange}
+              />
+              <span>{errors.repassword}</span>
+            </div>
+          </div>
+          <div className="action">
+            <button>SIGN UP</button>
+          </div>
+        </form>
+      </div>
+      <div className="error_msg">{signUpMessage}</div>
+    </section>
+  );
+};
 
 export default SignUp;
