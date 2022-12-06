f1eb6f704eed763f21f17e836701fabe5d3e8e8f
krzysztofkucharzyk <krzysztofkucharzyk@gmail.com>
2022-12-06 10:42:17 +0100

    STEP 10.v.0.5

--- src/views/Popup.js
+++ src/views/Popup.js
@@ -2,9 +2,9 @@ import React from 'react'
 import './Popup.css'
 import LoginForm from '../components/LoginForm';
 
-
 const Popup = (props) => {
 
+  //if it's true popup will show
   return (props.timePopup) ? (
     <div className='popup'>
       <LoginForm timePopup={props.timePopup} setTimePopup={props.setTimePopup} user={props.user} setUser={props.setUser}/>
